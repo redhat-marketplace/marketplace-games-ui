49ebdd6d3b277d6d18d9fd382fec72191d7d3cf9
Adams Ombonga <adamsombonga@gmail.com>
2020-10-05 17:08:03 -0400

    feat(Button): add btn component + unit-tests

---
+++ src/components/Button.spec.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/svelte';
+
+import Button from './Button.svelte';
+
+describe('<Button />', function () {
+  const btnCases =  [['primary'], ['secondary'], ['danger'], [undefined]]
+
+  test.each(btnCases)(
+    "should render a %p <button /> element w/SVG and correct styling",
+    (btnType) => {
+        const TEST_PROPS = {
+          type: btnType
+        };
+        const { container } = render(Button, { ...TEST_PROPS });
+        const btn = container.querySelector('Button');
+        const svg = container.querySelector('svg');
+        if (!btnType) btnType = 'primary';
+        expect(svg).toBeVisible();
+        expect(btn).toHaveClass(btnType);
+    }
+  )
+});
