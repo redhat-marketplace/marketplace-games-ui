f3dbf46ac9b9728a01ad23574cf5b307c5cf5f56
Adams Ombonga <adamsombonga@gmail.com>
2020-10-06 12:42:17 -0400

    feat(Checkbox): add checkbox component + unit-tests

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let label = 'Checkbox label';
+  export let checked = true;
+</script>
+
+<style>
+  label {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  input[type="checkbox"] {
+    box-sizing: border-box;
+    appearance: none;
+    background: transparent;
+    outline: 2px solid #011634;
+    outline-offset: -4px;
+    border: 3px solid #3e8bad;
+    width: 24px;
+    height: 24px;
+  }
+
+  input[type="checkbox"]:checked {
+    background: #13f3fe;
+  }
+
+  input,
+  span {
+    vertical-align: middle;
+  }
+
+  span {
+    margin-top: 15px;
+  }
+
+  .checkmark {
+    display: inline-block;
+  }
+
+  input[type="checkbox"]:checked + .checkmark:after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 14px;
+    top: 8px;
+    width: 6px;
+    height: 12px;
+    border: solid #043855;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+  }
+</style>
+
+<label>
+  <input type="checkbox" bind:checked class="input" on:change />
+  <span class="checkmark" />
+  {label}
+</label>
