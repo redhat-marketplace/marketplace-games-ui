b62818851dec40afb00a98c10c67859f39d6fc6d
zevioso <joshbuselt@gmail.com>
2020-10-12 11:46:51 -0400

    feat: placeholder for difficulty

---
+++ src/components/Difficulty/Difficulty.svelte
@@ -0,0 +1,15 @@
+<style>
+  .help-text {
+    font-size: 0.75rem;
+  }
+</style>
+
+<script>
+  import Block from '../Block/Block.svelte';
+</script>
+
+<Block title="Difficulty">
+  <p class="help-text" data-testid="help-text">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+  </p>
+</Block>
