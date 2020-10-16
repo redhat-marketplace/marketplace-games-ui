4a377a6da285cbfa621c4ac5185818b544f11841
zevioso <joshbuselt@gmail.com>
2020-10-16 08:42:12 -0400

    test: fixing merge conflict

--- src/App.spec.js
+++ src/App.spec.js
@@ -15,14 +15,4 @@ describe('<App />', function () {
   it('should render a game', function () {
     expect(screen.getByTestId('game-board')).toBeInTheDocument();
   });
-
-  it('should hide the Start button after a user clicks', async function () {
-    const button = screen.getByRole('button', { name: /Start/i });
-
-    await fireEvent.click(button);
-
-    expect(
-      screen.queryByRole('button', { name: /Start/i })
-    ).not.toBeInTheDocument();
-  });
 });
