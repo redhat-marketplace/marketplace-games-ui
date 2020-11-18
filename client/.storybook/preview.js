de017110b2d0cdee847e2e9406cd8fb5d28f9b23
zevioso <joshbuselt@gmail.com>
2020-11-18 11:43:21 -0500

    fix: updating bundled css file path

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,5 +1,5 @@
 import globalCss from '../public/global.css';
-import bundledCss from '../public/build/bundle.css';
+import bundledCss from '../public/bundle.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
